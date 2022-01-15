421be1127dfe49d30c2f88aaa727c5d54fabec52
SAMEER KULKARNI <74544735+sunbird-sameer@users.noreply.github.com>
2022-01-15 09:36:13 +0530

    Update night_light.py

--- night_light.py
+++ night_light.py
@@ -7,6 +7,9 @@ cv2.imshow("Image",image)
 alpha_beta = 3.5
 
 added_image = cv2.addWeighted(image,alpha_beta,image,alpha_beta,0)
+added_image = cv2.GaussianBlur(added_image,(5,5),cv2.BORDER_DEFAULT)
+added_image = cv2.detailEnhance(added_image, sigma_s=1, sigma_r=10000)
 
 cv2.imshow("Brightend Image", added_image)
+cv2.imwrite("Brightend Image.png", added_image)
 
